3bb702e6951f1bef3223de3a40c8a2ef24c183cb
Jacob Neher <services@threeshadowsmedia.com>
2013-07-17 15:39:32 -0600

    Added support for displaying the current height and width of the window.

--- modules/skrollr_ui/js/skrollr_ui.js
+++ modules/skrollr_ui/js/skrollr_ui.js
@@ -1,13 +1,19 @@
 (function ($) {
   Drupal.behaviors.skrollr_ui = {
-    attach: function(context, settings) {
-    // Cache the Window object
-	$window = $(window);
-	// Get the inital value when the page is loaded...
-    $('#skrollr-ui-indicator').html($window.scrollTop() + 'px');
+	attach: function(context, settings) {
+	// Cache the Window object
+    $window = $(window);
+	// Get the inital values when the page is loaded...
+	$('#skrollr-ui-scroll-indicator').html($window.scrollTop() + 'px');
+	$('#skrollr-ui-height-indicator').html($window.height() + 'px');
+	$('#skrollr-ui-width-indicator').html($window.width() + 'px');
 	$window.scroll(function() {
 	  // ...then get the value as the page is actively scrolled
-      $('#skrollr-ui-indicator').html($window.scrollTop() + 'px');
+      $('#skrollr-ui-scroll-indicator').html($window.scrollTop() + 'px');
+	});
+	$window.resize(function() {
+	  $('#skrollr-ui-height-indicator').html($window.height() + 'px');
+	  $('#skrollr-ui-width-indicator').html($window.width() + 'px');
 	});
     }
   }
