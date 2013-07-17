9348f1e5ae9567cacd842e72013f294daf8cf2b6
Jacob Neher <services@threeshadowsmedia.com>
2013-07-17 15:46:35 -0600

    Added functionality to hide the scroll indicator when not on a Skrollr page (.view-id-skrollr only currently) since the scroll info is useless on other page.

--- modules/skrollr_ui/js/skrollr_ui.js
+++ modules/skrollr_ui/js/skrollr_ui.js
@@ -3,15 +3,25 @@
 	attach: function(context, settings) {
 	// Cache the Window object
     $window = $(window);
-	// Get the inital values when the page is loaded...
-	$('#skrollr-ui-scroll-indicator').html($window.scrollTop() + 'px');
+	// Get the inital values for height and width when the page is loaded...
 	$('#skrollr-ui-height-indicator').html($window.height() + 'px');
 	$('#skrollr-ui-width-indicator').html($window.width() + 'px');
-	$window.scroll(function() {
-	  // ...then get the value as the page is actively scrolled
-      $('#skrollr-ui-scroll-indicator').html($window.scrollTop() + 'px');
-	});
+	
+	// We only want to add the scroll indicator if we are on a skrollr page...otherwise it's kinda useless :)
+	if ($('.view-id-skrollr').length) {
+	  // Set the initial value
+	  $('#skrollr-ui-scroll-indicator').html($window.scrollTop() + 'px');
+	  $window.scroll(function() {
+		// ...then get the value as the page is actively scrolled
+		$('#skrollr-ui-scroll-indicator').html($window.scrollTop() + 'px');
+	  });
+	}
+	else {
+	  // Add the 'element-invisible' class to hide the box
+	  $('#skrollr-ui-scroll-indicator').addClass('element-invisible');
+	}
 	$window.resize(function() {
+	  // ... then get the value when the window is resized
 	  $('#skrollr-ui-height-indicator').html($window.height() + 'px');
 	  $('#skrollr-ui-width-indicator').html($window.width() + 'px');
 	});
